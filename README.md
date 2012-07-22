b78598cc860d6f495ee4c3e18312be6ca7865def
Bert Frees <bertfrees@gmail.com>
2012-07-22 15:22:33 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+[liblouis-java][]
+=================
+
+Java bindings to [liblouis][], an open-source braille translator and back-translator.
+The bindings are based on [JNA][].
+
+Building
+--------
+
+`ant jar` will create liblouis-java.jar
+
+Authors
+-------
+
++ [Bert Frees](http://github.com/bertfrees)
+
+[liblouis-java]: http://github.com/bertfrees/liblouis-java
+[liblouis]: http://code.google.com/p/liblouis/
+[jna]: https://github.com/twall/jna
---
+++ src/org/liblouis/Translator.java
@@ -0,0 +1,41 @@
+package org.liblouis;
+
+import com.sun.jna.ptr.IntByReference;
+
+public class Translator {
+
+	private static final LouisLibrary LIBLOUIS = LouisLibrary.INSTANCE;
+
+	private static WideString INPUT_BUFFER = new WideString(512);
+	private static WideString OUTPUT_BUFFER = new WideString(1024);
+	
+	private final String tables;
+	
+	public Translator(String tables) {
+		this.tables = tables;
+	}
+
+	public TranslationResult translate(String text) throws Exception {
+		
+		try {
+			INPUT_BUFFER.write(text);
+		} catch (IllegalArgumentException e) {
+			INPUT_BUFFER = new WideString(text);
+		}
+		final IntByReference inLen = new IntByReference(text.length());
+		final IntByReference outLen = new IntByReference();
+		
+		while (true) {
+			outLen.setValue(OUTPUT_BUFFER.length());
+			if (LIBLOUIS.lou_translate(tables, INPUT_BUFFER, inLen, OUTPUT_BUFFER, outLen,
+					null, null, null, null, null, 0) == 0) {
+				throw new RuntimeException("Unable to complete translation");
+			}
+			boolean outputBufferFull = (outLen.getValue() >= OUTPUT_BUFFER.length());
+			if (!outputBufferFull) { break; }
+			OUTPUT_BUFFER = new WideString(2 * OUTPUT_BUFFER.length());
+		}
+		
+		return new TranslationResult(OUTPUT_BUFFER, outLen);
+	}
+}
