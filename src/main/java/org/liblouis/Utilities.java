f9a1fa48678a8065141f04054deeabc1db0b7463
Bert Frees <bertfrees@gmail.com>
2012-11-16 19:56:36 +0100

    Utilities class instead of org.liblouis.util package

--- src/main/java/org/liblouis/Utilities.java
+++ src/main/java/org/liblouis/Utilities.java
@@ -1,20 +1,23 @@
-package org.liblouis.util;
+package org.liblouis;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
 
-public class Environment {
-
-	public static void setLouisTablePath(String value) {
-		if (libc instanceof UnixCLibrary) {
-			((UnixCLibrary)libc).setenv("LOUIS_TABLEPATH", value, 1); return;
-		} else {
-			((WindowsCLibrary)libc)._putenv("LOUIS_TABLEPATH=");
-			((WindowsCLibrary)libc)._putenv("LOUIS_TABLEPATH=" + value);
+public class Utilities {
+	
+	public static class Environment {
+		
+		public static void setLouisTablePath(String value) {
+			if (libc instanceof UnixCLibrary) {
+				((UnixCLibrary)libc).setenv("LOUIS_TABLEPATH", value, 1); return;
+			} else {
+				((WindowsCLibrary)libc)._putenv("LOUIS_TABLEPATH=");
+				((WindowsCLibrary)libc)._putenv("LOUIS_TABLEPATH=" + value);
+			}
 		}
 	}
-
+	
 	private static Object libc;
 	
 	static {
@@ -28,11 +31,11 @@ public class Environment {
 				break;
 		}
 	}
-
+	
 	public interface UnixCLibrary extends Library {
 		public int setenv(String name, String value, int overwrite);
 	}
-
+	
 	public interface WindowsCLibrary extends Library {
 		public int _putenv(String string);
 	}
