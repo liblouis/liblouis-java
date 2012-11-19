7420fc1dccf817161626f388647acf33204077f6
Bert Frees <bert.frees@sbs.ch>
2012-11-19 14:18:49 +0100

    Add tests

--- src/main/java/org/liblouis/Utilities.java
+++ src/main/java/org/liblouis/Utilities.java
@@ -1,10 +1,53 @@
 package org.liblouis;
 
+import java.util.Arrays;
+
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
 
 public class Utilities {
+	
+	public static class Pair<T1,T2> {
+		public final T1 _1;
+		public final T2 _2;
+		public Pair(T1 _1, T2 _2) {
+			this._1 = _1;
+			this._2 = _2;
+		}
+	}
+	
+	public static class Hyphenation {
+		
+		public static Pair<String,boolean[]> extractHyphens(String string, char hyphen) {
+			StringBuffer unhyphenatedString = new StringBuffer();
+			boolean[] hyphens = new boolean[string.length()/2];
+			int j = 0;
+			boolean seenHyphen = false;
+			for (int i = 0; i < string.length(); i++) {
+				char c = string.charAt(i);
+				if (c == hyphen)
+					seenHyphen = true;
+				else
+					unhyphenatedString.append(c);
+					hyphens[j++] = seenHyphen;
+					seenHyphen = false; }
+			return new Pair<String,boolean[]>(unhyphenatedString.toString(), Arrays.copyOf(hyphens, j-1));
+		}
+		
+		public static String insertHyphens(String string, boolean hyphens[], char hyphen) {
+			if (string.equals("")) return "";
+			if (hyphens.length != string.length()-1)
+				throw new RuntimeException("hyphens.length must be equal to string.length() - 1");
+			StringBuffer hyphenatedString = new StringBuffer();
+			int i; for (i = 0; i < hyphens.length; i++) {
+				hyphenatedString.append(string.charAt(i));
+				if (hyphens[i])
+					hyphenatedString.append(hyphen); }
+			hyphenatedString.append(string.charAt(i));
+			return hyphenatedString.toString();
+		}
+	}
 	
 	public static class Environment {
 		
---
+++ src/test/java/org/liblouis/TranslatorTest.java
@@ -0,0 +1,72 @@
+package org.liblouis;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.apache.commons.io.filefilter.FileFilterUtils.asFileFilter;
+import static org.apache.commons.io.filefilter.FileFilterUtils.trueFileFilter;
+import static org.junit.Assert.assertEquals;
+import static org.liblouis.Utilities.Hyphenation.insertHyphens;
+
+public class TranslatorTest {
+	
+	@Test
+	public void testVersion() {
+		assertEquals(
+			"2.6.0-SNAPSHOT",
+			Louis.getLibrary().lou_version());
+	}
+	
+	@Test
+	public void testTranslate() {
+		Translator translator = newTranslator("foobar.cti,foobar.dic");
+		assertEquals(
+			"foobar",
+			translator.translate("foobar", null, null, false).getBraille());
+	}
+	
+	@Test
+	public void testHyphenate() {
+		Translator hyphenator = newTranslator("foobar.cti,foobar.dic");
+		assertEquals(
+			"foo-bar",
+			insertHyphens("foobar", hyphenator.hyphenate("foobar"), '-'));
+	}
+	
+	@Test
+	public void testTranslateAndHyphenate() {
+		Translator translator = newTranslator("foobar.cti,foobar.dic");
+		TranslationResult result = translator.translate("foobar", null, null, true);
+		assertEquals(
+				"foo-bar",
+				insertHyphens(result.getBraille(), result.getHyphenPositions(), '-'));
+	}
+	
+	private Translator newTranslator(String tables) {
+		try {
+			return new Translator(new File(tablesDir, tables).getCanonicalPath()); }
+		catch (IOException e) {
+			throw new RuntimeException(e); }
+	}
+	
+	private File tablesDir;
+	
+	@Before
+	@SuppressWarnings("unchecked")
+	public void initialize() {
+		File testRootDir = new File(this.getClass().getResource("/").getPath());
+		tablesDir = new File(testRootDir, "tables");
+		Louis.setLibraryPath(((Collection<File>)FileUtils.listFiles(
+				new File(testRootDir, "../nar"),
+				asFileFilter(new FilenameFilter() {
+					public boolean accept(File dir, String fileName) {
+						return dir.getName().equals("shared") && fileName.startsWith("liblouis"); }}),
+				trueFileFilter())).iterator().next());
+	}
+}
