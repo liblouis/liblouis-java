85cdf038816fe3473c18dad1eee6260a4081f6d7
Bert Frees <bert.frees@sbs.ch>
2014-06-02 18:39:05 +0200

    Prepare for release 1.2.0

--- src/test/java/org/liblouis/TableResolverTest.java
+++ src/test/java/org/liblouis/TableResolverTest.java
@@ -1,6 +1,12 @@
 package org.liblouis;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Collection;
+
+import static org.apache.commons.io.filefilter.FileFilterUtils.asFileFilter;
+import static org.apache.commons.io.filefilter.FileFilterUtils.trueFileFilter;
+import org.apache.commons.io.FileUtils;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,10 +31,16 @@ public class TableResolverTest {
 			translator.translate("foobar", null, null).getBraille());
 	}
 	
-	private final File testRootDir = new File(this.getClass().getResource("/").getPath());
-	
 	@Before
-	public void registerMagicTokenResolver() {
+	@SuppressWarnings("unchecked")
+	public void initialize() {
+		final File testRootDir = new File(this.getClass().getResource("/").getPath());
+		Louis.setLibraryPath(((Collection<File>)FileUtils.listFiles(
+				new File(testRootDir, "../dependency"),
+				asFileFilter(new FilenameFilter() {
+					public boolean accept(File dir, String fileName) {
+						return dir.getName().equals("shared") && fileName.startsWith("liblouis"); }}),
+				trueFileFilter())).iterator().next());
 		Louis.getLibrary().lou_registerTableResolver(
 			new TableResolver() {
 				public File[] invoke(String table, File base) {
--- src/test/java/org/liblouis/TranslatorTest.java
+++ src/test/java/org/liblouis/TranslatorTest.java
@@ -19,7 +19,7 @@ public class TranslatorTest {
 	@Test
 	public void testVersion() {
 		assertEquals(
-			"2.5.3",
+			"2.5.4",
 			Louis.getLibrary().lou_version());
 	}
 	
