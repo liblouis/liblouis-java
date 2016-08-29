fdf5937e881660b47ddd8f4fc8ede9a91e39fe3b
Bert Frees <bertfrees@gmail.com>
2016-08-29 19:53:00 +0200

    Put some shared code in helper class

--- src/test/java/org/liblouis/FindTranslatorTest.java
+++ src/test/java/org/liblouis/FindTranslatorTest.java
@@ -1,12 +1,6 @@
 package org.liblouis;
 
 import java.io.File;
-import java.io.FilenameFilter;
-import java.util.Collection;
-
-import static org.apache.commons.io.filefilter.FileFilterUtils.asFileFilter;
-import static org.apache.commons.io.filefilter.FileFilterUtils.trueFileFilter;
-import org.apache.commons.io.FileUtils;
 
 import org.junit.Test;
 
@@ -33,13 +27,8 @@ public class FindTranslatorTest {
 	
 	@SuppressWarnings("unchecked")
 	public FindTranslatorTest() {
-		File testRootDir = new File(this.getClass().getResource("/").getPath());
-		Louis.setLibraryPath(((Collection<File>)FileUtils.listFiles(
-				new File(testRootDir, "../dependency"),
-				asFileFilter(new FilenameFilter() {
-					public boolean accept(File dir, String fileName) {
-						return dir.getName().equals("shared") && fileName.startsWith("liblouis"); }}),
-				trueFileFilter())).iterator().next());
+		Helper.setLibraryPath();
+		File testRootDir = new File(Helper.class.getResource("/").getPath());
 		File[] tables = new File(testRootDir, "tables").listFiles();
 		String[] tableNames = new String[tables.length];
 		for (int i = 0; i < tableNames.length; i++)
--- src/test/java/org/liblouis/TranslatorTest.java
+++ src/test/java/org/liblouis/TranslatorTest.java
@@ -1,16 +1,11 @@
 package org.liblouis;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.Collection;
 
-import org.apache.commons.io.FileUtils;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-import static org.apache.commons.io.filefilter.FileFilterUtils.asFileFilter;
-import static org.apache.commons.io.filefilter.FileFilterUtils.trueFileFilter;
-import static org.junit.Assert.assertEquals;
 import static org.liblouis.Utilities.Hyphenation.insertHyphens;
 
 public class TranslatorTest {
@@ -104,13 +99,8 @@ public class TranslatorTest {
 
 	@SuppressWarnings("unchecked")
 	public TranslatorTest() {
+		Helper.setLibraryPath();
 		File testRootDir = new File(this.getClass().getResource("/").getPath());
 		tablesDir = new File(testRootDir, "tables");
-		Louis.setLibraryPath(((Collection<File>)FileUtils.listFiles(
-				new File(testRootDir, "../dependency"),
-				asFileFilter(new FilenameFilter() {
-					public boolean accept(File dir, String fileName) {
-						return dir.getName().equals("shared") && fileName.startsWith("liblouis"); }}),
-				trueFileFilter())).iterator().next());
 	}
 }
